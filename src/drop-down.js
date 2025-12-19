8da261cad69d994338e82cab93b752ca0369ec40
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2025-12-19 18:25:28 +0900

    Add drop-down

--- src/drop-down.js
+++ src/drop-down.js
@@ -1,1 +1,38 @@
-export const test = () => console.log("drop-down");
+const dropDown = document.createElement("div");
+dropDown.classList.add("drop-down");
+
+export function renderDropDown() {
+  const btnDropDown = document.createElement("button");
+  btnDropDown.classList.add("show");
+  btnDropDown.type = "button";
+  btnDropDown.textContent = "Drop Down Button";
+
+  const dropDownContent = renderDropDownContent();
+
+  dropDown.append(btnDropDown, dropDownContent);
+}
+
+function renderDropDownContent() {
+  const dropDownContent = document.createElement("div");
+  dropDownContent.classList.add("content", "hide");
+
+  const dropDownList = document.createElement("ul");
+  dropDownList.classList.add("list");
+
+  for (let i = 0; i < 3; i++) {
+    const listItem = document.createElement("li");
+    listItem.classList.add("list-item");
+    listItem.textContent = `Item ${i}`;
+    dropDownList.append(listItem);
+  }
+
+  dropDownContent.append(dropDownList);
+
+  return dropDownContent;
+}
+
+export function getDropDownContent() {
+  return dropDown.querySelector(".content");
+}
+
+export default dropDown;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import { test } from "./drop-down.js";
+import dropDown, { getDropDownContent, renderDropDown } from "./drop-down";
+import "./styles.css";
 
-test();
+const body = document.body;
+
+function init() {
+  renderDropDown();
+  body.append(dropDown);
+}
+
+dropDown.addEventListener("click", (e) => {
+  if (e.target.matches("button.show")) {
+    const dropDownContent = getDropDownContent();
+    dropDownContent.classList.toggle("hide");
+  }
+});
+
+init();
